59a82b43f48ac06cb2f9a83a13bbfc214c51b0c1
flydev-fr <code@sekretservices.com>
2022-01-05 11:22:48 +0100

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+import { onMount } from 'svelte'
+import DailyIframe from '@daily-co/daily-js'
+
+
+onMount(async () => {
+  let callObject = DailyIframe.createFrame();
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+  let recorder = new MediaRecorder(stream)
+  recorder.start()
+})
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
